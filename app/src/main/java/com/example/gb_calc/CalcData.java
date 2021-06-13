b2e389aa59be16aa0d097ef24c890e40c7f4fbe6
warcerg <warcerg@gmail.com>
2021-06-13 19:17:54 +0300

    практическое задание по уроку 3.
    задание доделано

--- app/src/main/java/com/example/gb_calc/CalcData.java
+++ app/src/main/java/com/example/gb_calc/CalcData.java
@@ -1,18 +1,13 @@
 package com.example.gb_calc;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-import android.widget.TextView;
-
 public class CalcData {
-    private TextView textInput;
-    private TextView textResult;
+    private String input = "";
+    private String result = "";
 
-    private boolean operationSum = false;
-    private boolean operationSubstract = false;
-    private boolean operationMultiplication = false;
-    private boolean operationDivision = false;
-    private boolean pointCheck = false;
+    private final String ZERO_ERROR = "ERROR. cannot divide on zero!";
+
+    private Operation operation = Operation.EMPTY;
+    private boolean pointCheck;
 
     private float valueOne;
     private float valueTwo;
@@ -20,59 +15,7 @@ public class CalcData {
     private int operPos;
 
     public CalcData() {
-        operationSum = false;
-        operationSubstract = false;
-        operationMultiplication = false;
-        operationDivision = false;
         pointCheck = false;
-    }
-
-    public TextView getTextInput() {
-        return textInput;
-    }
-
-    public void setTextInput(TextView textInput) {
-        this.textInput = textInput;
-    }
-
-    public TextView getTextResult() {
-        return textResult;
-    }
-
-    public void setTextResult(TextView textResult) {
-        this.textResult = textResult;
-    }
-
-    public boolean isOperationSum() {
-        return operationSum;
-    }
-
-    public void setOperationSum(boolean operationSum) {
-        this.operationSum = operationSum;
-    }
-
-    public boolean isOperationSubstract() {
-        return operationSubstract;
-    }
-
-    public void setOperationSubstract(boolean operationSubstract) {
-        this.operationSubstract = operationSubstract;
-    }
-
-    public boolean isOperationMultiplication() {
-        return operationMultiplication;
-    }
-
-    public void setOperationMultiplication(boolean operationMultiplication) {
-        this.operationMultiplication = operationMultiplication;
-    }
-
-    public boolean isOperationDivision() {
-        return operationDivision;
-    }
-
-    public void setOperationDivision(boolean operationDivision) {
-        this.operationDivision = operationDivision;
     }
 
     public boolean isPointCheck() {
@@ -107,13 +50,139 @@ public class CalcData {
         this.operPos = operPos;
     }
 
-    public void appendText(TextView textView, String string){
-        textView.append(string);
+    public String getInput() {
+        return input;
     }
 
-    public void setText(TextView textView, String string){
-        textView.setText(string);
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public void appendInput(String appendText) {
+        StringBuilder sb = new StringBuilder(input);
+        sb.append(appendText);
+        input = sb.toString();
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public void emptyCheck() {
+        if (input.isEmpty()) {
+            appendInput("0");
+        }
+    }
+
+    public void lastCharCheck() {
+        if (input.endsWith(Operation.PLUS.toString()) ||
+                input.endsWith(Operation.MINUS.toString()) ||
+                input.endsWith(Operation.MULTIPLY.toString()) ||
+                input.endsWith(Operation.DIVIDE.toString()) ||
+                input.endsWith(Operation.POINT.toString())) {
+            appendInput("0");
+        }
+    }
+
+    public void clearFields() {
+        setInput("");
+        setResult("");
+        setPointCheck(false);
+        operation = Operation.EMPTY;
+    }
+
+    public boolean operationCheck() { //смысл?
+        boolean check = true;
+        lastPointCheck();
+        String inputCheck = input.substring(0, input.length() - 1);
+        if (inputCheck.endsWith(Operation.PLUS.toString()) ||
+                inputCheck.endsWith(Operation.MINUS.toString()) ||
+                inputCheck.endsWith(Operation.MULTIPLY.toString()) ||
+                inputCheck.endsWith(Operation.DIVIDE.toString())) {
+            check = false;
+        }
+        return check;
+    }
+
+    public void lastPointCheck() {
+        if (input.endsWith(Operation.POINT.toString())) {
+            appendInput("0");
+        }
+    }
+
+    public void pointInput(){
+        emptyCheck();
+        lastPointCheck();
+        if(!isPointCheck()){
+            appendInput(Operation.POINT.toString());
+            setPointCheck(true);
+        }
+    }
+
+    public void operationChange(Operation oper) {
+        emptyCheck();
+        lastPointCheck();
+        if (input.endsWith(Operation.PLUS.toString()) ||
+                input.endsWith(Operation.MINUS.toString()) ||
+                input.endsWith(Operation.MULTIPLY.toString()) ||
+                input.endsWith(Operation.DIVIDE.toString())) {
+            input = input.substring(0, input.length() - 1);
+            operation = Operation.EMPTY;
+        }
+        if (operation == Operation.EMPTY){
+                operationInputUpd(oper);
+        }
     }
 
 
+    private void operationInputUpd(Operation oper) {
+        emptyCheck();
+        valueOne = Float.parseFloat(input);
+        appendInput(oper.toString());
+        operPos = input.length();
+        setPointCheck(false);
+        operation = oper;
+    }
+
+    public void operationEquals(){
+        emptyCheck();
+        lastPointCheck();
+        setResult("");
+        valueTwo = Float.parseFloat(input.substring(operPos));
+        switch (operation) {
+            case PLUS:
+                setResult(Float.toString(valueOne + valueTwo));
+                setInput("");
+                break;
+            case MINUS:
+                setResult(Float.toString(valueOne - valueTwo));
+                setInput("");
+                break;
+            case MULTIPLY:
+                setResult(Float.toString(valueOne * valueTwo));
+                setInput("");
+                break;
+            case DIVIDE:
+                if (valueTwo == 0) {
+                 setResult(ZERO_ERROR);
+                } else {
+                    setResult(Float.toString(valueOne / valueTwo));
+                }
+                setInput("");
+        }
+        setPointCheck(false);
+        operation = Operation.EMPTY;
+    }
 }
